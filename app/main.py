7d3c76c0ed93618b5147f0c1a506a0a2b4a6ddc1
n0sat1y <sashamorozov533@gmail.com>
2025-03-02 13:43:27 +0300

    add .gitignore

--- app/main.py
+++ app/main.py
@@ -16,8 +16,7 @@ app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(
-        "main:app",
-        host="127.0.0.1",  # localhost
+        "main:app",  # localhost
         port=8000,
         reload=True  # enable auto-reload
     )
