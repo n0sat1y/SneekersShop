71dc58d29132f046db3932638e04fdc4b8b6f183
n0sat1y <sashamorozov533@gmail.com>
2025-03-13 19:16:14 +0300

    tablename format change

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
 from fastapi import Depends
 
 from .settings import settings
@@ -17,8 +18,19 @@ async def session():
 
 SessionDep = Annotated[AsyncSession, Depends(session)]
 
+convention = {
+    "all_column_names": lambda constraint, table: "_".join(
+        [column.name for column in constraint.columns.values()]
+    ),
+    "ix": "ix__%(table_name)s__%(all_column_names)s",
+    "uq": "uq__%(table_name)s__%(all_column_names)s",
+    "ck": "ck__%(table_name)s__%(constraint_name)s",
+    "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
+    "pk": "pk__%(table_name)s",
+}
+
 class Base(DeclarativeBase):
-	pass
+	metadata = MetaData(naming_convention=convention)
 
 
 async def start_db():
