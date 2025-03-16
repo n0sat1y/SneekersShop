60f0e6dadf4e6766b43ee7d54b4752cf15111d3f
n0sat1y <sashamorozov533@gmail.com>
2025-03-16 08:58:33 +0300

    precommit models/__init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,25 @@
 from datetime import datetime
 from typing import Annotated
-from sqlalchemy import text, DateTime, TIMESTAMP
+from sqlalchemy import text, TIMESTAMP
 from sqlalchemy.orm import mapped_column
 
+# Определяем базовые типы
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[
+    datetime,
+    mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=text("TIMEZONE('UTC', now())")
+    )
+]
+
+# Сначала импортируем модели
+from .products import ProductModel
+from .users import UserModel, ReviewModel
+from .orders import OrderModel, OrderItemsModel
+from .carts import CartModel, CartItemsModel
+
+# Потом объявляем что экспортировать
 __all__ = [
     'ProductModel',
     'UserModel', 
@@ -11,20 +28,4 @@ __all__ = [
     'CartItemsModel',
     'OrderModel',
     'OrderItemsModel',
-    'intpk',
-    'created_at'
 ]
-
-intpk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[
-    datetime,
-    mapped_column(
-        server_default=text("TIMEZONE('UTC', now())")
-    )
-]
-
-# Import models after defining the types
-from .products import ProductModel
-from .users import UserModel, ReviewModel
-from .orders import OrderModel, OrderItemsModel
-from .carts import CartModel, CartItemsModel
